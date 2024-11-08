9f7a3f3bc96de5c243250fa4eb351cc9660108ac
M-u-qp <muqpu4eckui@gmail.com>
2024-11-08 15:32:45 +0300

    Added font and colors for theme

--- app/src/main/java/com/example/simplifiedcrm/ui/theme/Type.kt
+++ app/src/main/java/com/example/simplifiedcrm/ui/theme/Type.kt
@@ -2,33 +2,122 @@ package com.example.simplifiedcrm.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.simplifiedcrm.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    //BODY
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    //TITLE
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.2.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    //LABEL
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    //DISPLAY
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.0.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.2).sp
+    ),
+    //HEADLINE
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.plus_jakarta_sans_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.0.sp
     )
-    */
 )
